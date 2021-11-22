e7cb8933c31d0836cd0ae3787bef2e5cb31f1cf2
Mario Granados <mario.alberto.granados1@gmail.com>
2021-11-22 12:29:10 -0600

    added the classword

---
+++ primitive-types.js
@@ -0,0 +1,13 @@
+console.log(99.99);
+
+console.log("false");
+
+console.log(false);
+
+console.log('0');
+
+console.log(0);
+
+console.log(true);
+
+console.log('true');
---
+++ string.js
@@ -0,0 +1,7 @@
+var name = "mario\n",
+    intro = "Hi,\nmy name is ",
+    hobby = "I like to make music",
+    newSentence = intro + name + hobby;
+
+
+console.log(newSentence);
