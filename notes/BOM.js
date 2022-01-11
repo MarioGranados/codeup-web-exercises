f685b1711c706f7c787432b754409ddb20aada5e
Mario Granados <mario.alberto.granados1@gmail.com>
2022-01-11 15:14:31 -0600

    BOM

---
+++ notes/BOM.js
@@ -0,0 +1,29 @@
+"use strict"
+
+/*let favNum = 18;
+
+let add = (num) => {
+    return num + num;
+}
+
+console.log(
+add(favNum)
+);*/
+
+// this code will produce a console log every second
+// when count >= max, the interval is cancelled, and the logging will stop
+
+var count = 0;
+var max = 10;
+var interval = 1000; // interval time in milliseconds
+
+var intervalId = setInterval(function () {
+    if (count >= max) {
+        clearInterval(intervalId);
+        console.log('All done');
+    } else {
+        count++;
+        console.log('Repeating this line ' + count);
+    }
+}, interval);
+
