6ff4df664c84f6ec45919fbbd3df4a8592b27a16
Mario Granados <mario.alberto.granados1@gmail.com>
2022-01-19 09:19:33 -0600

    review

---
+++ notes/objects_review.js
@@ -0,0 +1,31 @@
+/*
+let USA = {
+    Texas: {
+        size: 'big',
+        flower: 'bluebonnet',
+        bird: 'mockingbird',
+        people: 30,
+
+    }
+}
+
+console.log(USA);*/
+
+
+let sirius = {
+    //properties
+    numStudents: 29,
+    city: 'San Antonio',
+    inPerson: false,
+    theRest: true,
+    //methods store functions
+
+    returnCity: function () {
+        //do something
+        console.log(8 +8);
+        console.log('hello World');
+        return this.city;
+    }
+}
+
+console.log(sirius.inPerson);
