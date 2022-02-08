948030feeba7d8b30f96eb5a3240887d82674368
Mario Granados <mario.alberto.granados1@gmail.com>
2022-02-08 15:15:13 -0600

    completed promises

--- promises.js
+++ promises.js
@@ -1,9 +1,16 @@
 let APIKEY = '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';
 let url = 'https://api.github.com/users/tater/events'
 
-fetch(url, {headers: {'Authorization': APIKEY}}).then(function (result) {
-    console.log(result)
-})
+"use strict";
+
+
+function getGithubUsernames(username) {
+    fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': APIKEY}})
+        .then(response => response.json())
+        .then(responseObj => console.log(responseObj[0].created_at))
+}
+
+getGithubUsernames('MarioGranados')
 
 
     let wait = num => new Promise( (resolve, reject) => {
