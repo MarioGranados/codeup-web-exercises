1417998d72e595e46b4fd460bb9f3efa78e4ce72
Mario Granados <mario.alberto.granados1@gmail.com>
2022-02-08 09:25:08 -0600

    updated

---
+++ warm-up-ex.js
@@ -0,0 +1,12 @@
+
+let arr = [2,3,5,8,3,1,2,6,3,2,7,8,2,4,7];
+function removeDuplicates(arr) {
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i] === arr[i+2]) {
+            duplicate++
+        } else {
+
+        }
+    }
+}
+
